01a4bc6a13db9f65cae88e15d020d6ac8ba949c5
ahmed helmy <helmyahmed248@gmail.com>
2021-11-10 14:55:34 +0200

    admin lte

--- src/pages/admin/Home.vue
+++ src/pages/admin/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="home">
-     wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-      
+  <div>
+    <top-bar></top-bar>
+    <div>
+      testing
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
  
-
+import TopBar from "@/pages/admin/layouts/topBar"
 export default {
+  components:{TopBar},
   name: 'Home',
  
 }
--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,38 +1,60 @@
 <template>
   
     <div>
-        <navbar/>
-        <div id="layoutSidenav">
-            <sidebar/>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                         <router-view name="adminPages"></router-view>
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
+         <div class="wrapper">
+
+  <!-- Preloader -->
+  <!-- <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div> -->
+
+  <!-- Navbar -->
+  <navbar/>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+ <sidebar/>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+     <!-- include top in pages -->
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <router-view name="adminPages"></router-view>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0-rc
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
         
     </div>
 
 </template>
 
 <script>
+ 
 import navbar from './layouts/navbar.vue'
 import sidebar from './layouts/sidebar.vue'
 export default {
+  
   components: { navbar,sidebar },
     name:"App",
 }
